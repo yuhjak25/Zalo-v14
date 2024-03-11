52f92aa88b9b382645e19894fe74c3c01ff897a8
yuhjak <163014030+yuhjak25@users.noreply.github.com>
2024-03-11 21:55:14 +0100

    Delete Zalo directory

--- Zalo/src/models/ticket-channel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const TicketConfigSchema = new mongoose.Schema({
-  guildId: String,
-  TicketChannelId: String,
-});
-
-const TicketConfig = mongoose.model("TicketConfig", TicketConfigSchema);
-
-module.exports = TicketConfig;
